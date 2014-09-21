a1611e3ff98389df9caaad60edc91ccdc3fba23f
hell <hell@hellgate.(none)>
2014-09-21 15:52:29 +0200

    Tasks queue implemented

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from threading import Thread
 from flask import Flask, render_template, session
 from flask.ext.socketio import SocketIO, emit
 import datetime
+import Queue
 
 from pprint import pprint
 import random
@@ -17,7 +18,7 @@ app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 thread = None
-
+task_queue = Queue.Queue(0)
 
 class Tasks(object):
     _tasks = {}
@@ -43,19 +44,17 @@ class Tasks(object):
         self.id = self._id[0]
         self.id += 1
         self._id[0] = self.id
-        print(self.id)
 
     def add_task(self, task):
         '''
         adding task to class the storage and emiting it to clients
         '''
-        print(task)
         if 'state' not in task:
             task['state'] = 'todo'
         if 'severity' not in task:
             task['severity'] = 5
         else:
-            task['severity'] = int(task['severity']) 
+            task['severity'] = int(task['severity'])
         if 'requestor' not in task:
             task['requestor'] = 'Automata'
         self.tasks[self.id] = task
@@ -85,29 +84,29 @@ class Tasks(object):
     @staticmethod
     def emit_task(id, task, one_user=False, modify=False):
         '''
-        id, severity, state, data, 
-        add timestamp
+        TODO:
+            add timestamp
+            ugly html generation, maybe templates would be better?
         '''
-        additional_html=''
+        html_elements=[]
         if task['state'] == 'todo':
             priority_colors = {1:'#FF0000', 2:'#DD3300', 3:'#DD8855', 4:'#DDAA77', 5:'#999999'}
             color = priority_colors[task['severity']]
-            additional_html=additional_html+''' Claim it: <input class="claim" type="submit" value="Claim">'''.format(id=id)
+            html_elements.append(''' Claim it: <input class="claim" type="submit" value="Claim">'''.format(id=id))
         elif task['state'] == 'done':
-            additional_html=additional_html+''' Solved by: {}'''.format(task['claimer'])
-            color = 'green'
+            html_elements.append(''' Solved by: {}'''.format(task['claimer']))
+            color = '#00CC00'
         elif task['state'] == 'ongoing':
-            additional_html=additional_html+''' Claimed by: {} Done: <input class="solved" type="submit" value="Done">'''.format(task['claimer'])
-            color = 'yellow'
+            html_elements.append(''' Claimed by: {} Done: <input class="solved" type="submit" value="Done">'''.format(task['claimer']))
+            color = '#DBFF70'
         else:
-            color = '00FFFF'
-
-        html_code = '''\
+            color = '#00FFFF'
+        html_elements.append('</div>')
+        html_elements.insert(0, '''\
 <div class="tasks" id='{id}' style="background:{color}"> \
-<br>New task is: {content}, id: {id}, priority: {priority}, Requestor: {requestor}'''.format(id=id, state=task['state'], priority=task['severity'], color=color, content=task['content'], requestor=task['requestor'])
-        end_code = html_code + additional_html + '</div>'
-        print(end_code)
-
+<br>New task is: {content}, id: {id}, priority: {priority}, Requestor: {requestor}'''.format(id=id, state=task['state'], priority=task['severity'], color=color, content=task['content'], requestor=task['requestor']))
+        end_code = ''.join(html_elements)
+        pprint(end_code)
         if one_user:
             if modify:
                 emit.emit('replace_task',  {'id':id, 'new_code':end_code})
@@ -123,26 +122,38 @@ class Tasks(object):
     def emit_all_tasks(self):
         for task in self._tasks:
             self.emit_task(task, self._tasks[task], one_user=True)
+    
 
 
 
-def background_thread():
-    """This thread should be an queue consumer
-    Consuming instances put into queue bar multiple other watchers"""
+def example_producer_thread():
+    global task_queue
     while True:
         line = 'Errytime the same!'
-        task_id = Tasks().add_task(
-                {'content': line, 'severity':random.randint(1, 5)})
-        print(task_id)
-        time.sleep(5)
+        task={'content': line, 'severity':random.randint(1, 5)}
+        pprint('Producing: ')
+        pprint(task)
+        task_queue.put(task)
+        time.sleep(random.randint(3, 8))
+
+def consumer_thread():
+    """This thread should be an queue consumer
+    Consuming instances put into queue bar multiple other watchers"""
+    global task_queue
+    while True:
+        while not task_queue.empty():
+            time.sleep(1)
+            task = task_queue.get()
+            pprint('Consuming: ')
+            pprint(task)
+            Tasks().add_task({'content': task['content'], 'severity':task['severity']})
+        time.sleep(1)
 
 
 @app.route('/')
 def index():
-    global thread
-    if thread is None:
-        thread = Thread(target=background_thread)
-        thread.start()
+
+
     return render_template('index.html')
 
 
@@ -153,13 +164,16 @@ def login(message):
 
 @socketio.on('take_task', namespace='/test')
 def propagate_take_task(message):
-    pprint(message)
     Tasks().modify_task(message['id'], {'claimer':message['user'], 'state':'ongoing'})
 
 @socketio.on('end_task', namespace='/test')
 def propagate_end_task(message):
-    pprint('#'*60)
     Tasks().modify_task(message['id'], {'state':'done'})
 
 if __name__ == '__main__':
+    producer = Thread(target=example_producer_thread)
+    producer.start()
+    consumer = Thread(target=consumer_thread)
+    consumer.start()
     socketio.run(app, port=9095, host='hellgate.pl')
+    
