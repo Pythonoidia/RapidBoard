0995161676009a4475b10e18660213886c629228
hell <hell@hellgate.(none)>
2014-09-21 17:11:59 +0200

    Added example emiter

--- emiters.py
+++ emiters.py
@@ -1,29 +1,20 @@
 
 
-
-class Emiters(object):
-    def __init__(self):
-        self.prev=None
-        self.filename='/home/teamspeak/teamspeak3-server_linux-x86/logs/ts3server_2014-07-28__16_46_55.187196_1.log', 'rb/home/teamspeak/teamspeak3-server_linux-x86/logs/ts3server_2014-07-28__16_46_55.187196_1.log'
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self.next()
-
-    def next(self):
-        
+class GetLastLineFromLog(object):
+    def __init__(self, queue, filename):
+        self.PREV=None
+        self.queue = queue
+        self.filename=filename
 
     def get_last_line(self):
         with open(self.filename, 'rb') as fh:
             fh.seek(-1024, 2)
             last_line = fh.readlines()[-1].decode()
-        if self.prev==last_line:
+        if self.PREV==last_line:
             pass
         else:
-            PREV=last_line
-            print(last_line)
+            self.PREV=last_line
+            self.queue.put({'content':last_line, 'severity':3})
 
 
 
