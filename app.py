16b92f82884dbc19089e4054a62dc6d052551a11
hell <hell@virtual.home>
2014-11-16 20:23:42 +0100

    removed not needed code

--- app.py
+++ app.py
@@ -77,116 +77,44 @@ def add_task(task):
         content=str(task['content']), severity=task['severity'],
         claimer='', requestor=task['requestor'], state=task['state']))
     db.session.commit()
-    Tasks().emit_task(task_id, task)
+    emit_task(task_id, task)
 
-def emit_tasks():
+def emit_all_tasks():
     for task in DBTasks.query.all():
-        print(task.__dict__)
-        #node_code = TaskHtml(task.__dict__, task.ID).generate()
-        Tasks().emit_task(task.ID, task.__dict__, one_user=True)
+        emit_task(task.ID, task.__dict__, one_user=True)
 
 def modify_task(task_id, new_data):
     '''
     new_data = {key:newvalue}
     '''
-    #DBTasks.query.filter(Clients.id == client_id_list).update({'status': status})
-    #task = db.session.query(DBTasks).filter(DBTasks.ID==task_id)
     DBTasks.query.filter_by(ID=task_id).update(new_data)
-    #task.update(new_data)
-    #for key in new_data.keys():
-    #    task.eval(key) = new_data[key]
     db.session.commit()
     task = DBTasks.query.filter_by(ID=task_id).first()
     if not task:
         print('for : {}'.format(task_id))
     else:
-        pprint(task)
-        pprint(task.__dict__)
-        print('for : {}'.format(task_id))
-        Tasks().emit_task(task_id, task.__dict__, modify=True)
+        emit_task(task_id, task.__dict__, modify=True)
 
 
 
-class Tasks(object):
-    _tasks = {}
+def emit_task(id, task, one_user=False, modify=False):
     '''
-    We are Borg.
-    ####Task:
-    'id': #unique
-    {timestamp:'date',
-    content:'information about task',
-    severity:'1-6', #where 1 is the highest, defines color of background
-    ####solver:'username',  NOT NEEDED ANYMORE, as claimer == solver
-    claimer:'username',
-    requestor:'username',
-    state:'done,ongoing,todo'
-
-    }
+    TODO:
+        add timestamp
+        ugly html generation, maybe templates would be better?
+        look at the top of the document
     '''
-    def __init__(self):
-        self.tasks = self._tasks
-        self.id = str(os.urandom(4).encode('hex'))
-
-    def add_task(self, task):
-        '''
-        adding task to class the storage and emiting it to clients
-        '''
-        if 'state' not in task:
-            task['state'] = 'todo'
-        if 'severity' not in task:
-            task['severity'] = 5
+    node_code = TaskHtml(task, id).generate()
+    if one_user:
+        if modify:
+            emit.emit('replace_task',  {'id':id, 'new_code':node_code})
         else:
-            task['severity'] = int(task['severity'])
-        if 'requestor' not in task:
-            task['requestor'] = 'Automata'
-        add_task(task)
-        self.tasks[self.id] = task
-        self.emit_task(self.id, task)
-        return(self.id)
-
-    def get_tasks(self):
-        '''
-        Returning all tasks which are stored in the class
-        '''
-        return(self.tasks)
-
-    def modify_task(self, id, new_data):
-        '''
-        new_data = {key:newvalue}
-        '''
-        for key in new_data.keys():
-            self.tasks[id][key] = new_data[key]
-        self.emit_task(id, self.tasks[id], modify=True)
-
-    @staticmethod
-    def get_timestamp():
-        return datetime.datetime.now()
-
-    @staticmethod
-    def emit_task(id, task, one_user=False, modify=False):
-        '''
-        TODO:
-            add timestamp
-            ugly html generation, maybe templates would be better?
-            look at the top of the document
-        '''
-        node_code = TaskHtml(task, id).generate()
-        if one_user:
-            if modify:
-                emit.emit('replace_task',  {'id':id, 'new_code':node_code})
-            else:
-                emit('add_task', {'new_code':node_code})
+            emit('add_task', {'new_code':node_code})
+    else:
+        if modify:
+            socketio.emit('replace_task',  {'id':id, 'new_code':node_code}, namespace='/test')
         else:
-            if modify:
-                socketio.emit('replace_task',  {'id':id, 'new_code':node_code}, namespace='/test')
-            else:
-                socketio.emit('add_task', {'new_code':node_code}, namespace='/test')
-            #emit('replace_task', {'id':id, 'new_code':end_code}, broadcast=True)
-
-    def emit_all_tasks(self):
-        for task in self._tasks:
-            self.emit_task(task, self._tasks[task], one_user=True)
-
+            socketio.emit('add_task', {'new_code':node_code}, namespace='/test')
 
 @app.route('/')
 def index():
@@ -203,22 +131,17 @@ def add_task_manually(msg):
 @socketio.on('username', namespace='/test')
 def login(message):
     emit('log', {'data': 'New user: '+ message['username']}, broadcast=True)
-    emit_tasks()
-    #Tasks().emit_all_tasks()
+    emit_all_tasks()
 
 @socketio.on('take_task', namespace='/test')
 def propagate_take_task(message):
     modify_task(message['id'], {'claimer':message['user'], 'state':'ongoing'})
-    #Tasks().modify_task(message['id'], {'claimer':message['user'], 'state':'ongoing'})
 
 @socketio.on('end_task', namespace='/test')
 def propagate_end_task(message):
-    #Tasks().modify_task(message['id'], {'state':'done'})
     modify_task(message['id'], {'state':'done'})
 
 if __name__ == '__main__':
     db.create_all()
-    #for task in DBTasks.query.all():
-    #    pprint(task.__dict__)
     socketio.run(app, port=9095, host='localhost')
 
